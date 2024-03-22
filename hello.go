761af4aabc85452a721e5efc2ff7d05c3a1c879f
joe-boadi <joeboadi1@gmail.com>
2024-03-22 09:15:07 +0000

    Fetch rsc.io package/text

--- hello.go
+++ hello.go
@@ -1,9 +1,15 @@
 package main
 
-import "fmt"
+// import: Package quote collects pithy sayings.
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 func main() {
-
 	fmt.Println("Hello, world")
-
+	//returns a Go proverb.
+	fmt.Println(quote.Go())
+	// Display: "Don't communicate by sharing memory, share memory by communicating"
 }

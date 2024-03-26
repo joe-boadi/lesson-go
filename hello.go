7a5e889b4072ef1bba7903af37df5905d716d15e
joe-boadi <joeboadi1@gmail.com>
2024-03-26 12:23:47 +0000

    Modify with myMap() and update array_()

--- hello.go
+++ hello.go
@@ -1,11 +1,13 @@
 package main
+
 // import: Package quote collects pithy sayings.
 import (
 	"fmt"
+
 	"rsc.io/quote"
 )
 
-// Variable Shadowing
+// Variable Shadowing.
 func example() {
 	x := 1
 	fmt.Println(x)
@@ -19,12 +21,27 @@ func example() {
 	fmt.Println(x)
 }
 
+// Array & for loop.
+func array_() {
+	//var arrayList []string ,slice
+	arrayList := []string{"Books", "Pens", "Pencils"}
+	for _, element := range arrayList {
+		fmt.Println(element)
+	}
+}
+
+func myMap() {
+	// myMap := make(map[string]int)
+	var myMap = map[string]int{"Joe": 17, "Eve": 16}
+	fmt.Printf("Joe has %v cards", myMap["Joe"])
+}
 func main() {
 	//returns statements from "rsc".
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Opt())
 	fmt.Println(quote.Glass())
-
 	example()
+	array_()
+	myMap()
 }

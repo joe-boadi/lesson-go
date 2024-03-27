6d47717b7134a84a98430ea3ce4312065ecb1295
joe-boadi <joeboadi1@gmail.com>
2024-03-27 11:02:49 +0000

    Test time duration for slicing

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 // import: Package quote collects pithy sayings.
 import (
 	"fmt"
+	"time"
 
 	"rsc.io/quote"
 )
@@ -11,13 +12,11 @@ import (
 func example() {
 	x := 1
 	fmt.Println(x)
-
 	if true {
 		fmt.Println(x)
 		x := 2
 		fmt.Println(x)
 	}
-
 	fmt.Println(x)
 }
 
@@ -31,17 +30,44 @@ func array_() {
 }
 
 func myMap() {
-	// myMap := make(map[string]int)
+	// myMap := make(map[string]int) "or"
 	var myMap = map[string]int{"Joe": 17, "Eve": 16}
-	fmt.Printf("Joe has %v cards", myMap["Joe"])
+	fmt.Printf("Joe has %v cards \n", myMap["Joe"])
+
+	var cards, ok = myMap["Jones"]
+	if ok {
+		fmt.Printf("Card is %v", cards)
+	} else {
+		fmt.Println("Invalid name")
+	}
 }
+
+func timeTaken(slice []int, n int) time.Duration {
+	t0 := time.Now()
+	for len(slice) < n {
+		slice = append(slice, 1)
+	}
+	return time.Since(t0)
+}
+
 func main() {
 	//returns statements from "rsc".
 	fmt.Println(quote.Go())
 	fmt.Println(quote.Hello())
 	fmt.Println(quote.Opt())
 	fmt.Println(quote.Glass())
+
+	fmt.Println("\n")
+
 	example()
 	array_()
 	myMap()
+
+	fmt.Println("\n")
+
+	n := 10000
+	testSlice := []int{}
+	testSlice2 := make([]int, 0, n)
+	fmt.Printf("Total time with pre-allocation: %v \n", timeTaken(testSlice2, n))
+	fmt.Printf("Total time without pre-allocation: %v \n", timeTaken(testSlice, n))
 }
